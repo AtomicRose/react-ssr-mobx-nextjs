06bcb6919deccc13acfde78d01192b00cba3e7ef
AtomicRose <baishenfang@hotmail.com>
2018-12-05 14:41:04 +0800

    update dialog

--- pages/components/atoms/Dialog.js
+++ pages/components/atoms/Dialog.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Icon } from 'COMPONENTS/at-ui'
+import { Icon, Button } from 'COMPONENTS/at-ui'
 import style from 'SCSS/components/atoms/dialog.scss'
 const themeIcon = {
     info: 'info-circle',
@@ -10,12 +10,77 @@ const themeIcon = {
     purple: 'tag',
     danger: 'error'
 }
+const closeType = {
+    DELAY: 'DELAY',
+    MAX_LIMIT: 'MAX_LIMIT',
+    OK: 'OK',
+    CANCEL: 'CANCEL',
+    MASK: 'MASK'
+}
+const destory = (div, props) => {
+    const id = div.getAttribute('id')
+    if (document.getElementById(id)) {
+        if (props.beforeClose && props.beforeClose(props.closeBy, props)) {
+            ReactDOM.unmountComponentAtNode(div)
+            div.parentNode.removeChild(div)
+            props.afterClose && props.afterClose(props.closeBy, props)
+        }
+    }
+}
+const renderDialog = (props) => {
+    let dialogContainer = document.getElementById(`dialog_${props.type}`)
+    if (!dialogContainer) {
+        dialogContainer = document.createElement('div')
+        dialogContainer.id = `dialog_${props.type}`
+        dialogContainer.className = style['at-dialog-' + props.type + '-container']
+        document.body.appendChild(dialogContainer)
+    }
+    let dialogDIV = document.createElement('div')
+    dialogDIV.id = `dialog_${props.type}_${new Date().getTime()}`
+    dialogContainer.appendChild(dialogDIV)
+    props.id = dialogDIV.id
+    props.element = dialogDIV
+    ReactDOM.render(<Dialog {...props} />, dialogDIV)
+    if (props.max && props.max > 0) {
+        if (dialogContainer.childNodes.length > props.max) {
+            props.closeBy = closeType['MAX_LIMIT']
+            destory(dialogContainer.firstChild, props)
+        }
+    }
+    if (props.delay && props.delay > 0) {
+        setTimeout(() => {
+            props.closeBy = closeType['DELAY']
+            destory(dialogDIV, props)
+        }, props.delay)
+    }
+    return dialogDIV
+}
+
 class Dialog extends React.Component {
     constructor(props) {
         super(props)
     }
+    handleClickOk(props) {
+        let p = { ...props }
+        p.closeBy = closeType['OK']
+        destory(p.element, p)
+    }
+    handleClickCancel(props) {
+        let p = { ...props }
+        p.closeBy = closeType['CANCEL']
+        destory(p.element, p)
+    }
+    handleClickMask(props) {
+        if (props.enableCloseByMask) {
+            let p = { ...props }
+            p.closeBy = closeType['MASK']
+            destory(p.element, p)
+        }
+    }
     render() {
         const theme = this.props.theme
+        const okBtnTheme = this.props.okBtnTheme || (theme === 'default' ? 'primary' : theme)
+        const cancelBtnTheme = this.props.cancelBtnTheme || 'default'
         switch (this.props.type) {
             case 'toast': {
                 return (
@@ -31,47 +96,42 @@ class Dialog extends React.Component {
                     </div>
                 )
             }
+            case 'alert': {
+                return (
+                    <div>
+                        {this.props.enableMask && <div className={style['dialog-mask']} onClick={() => this.handleClickMask(this.props)}></div>}
+                        <div className={`${style['at-dialog-alert']} ${style[theme]}`}>
+                            <div className={style['alert-title']}>{this.props.title}</div>
+                            <div className={style['alert-content']}>{this.props.content}</div>
+                            <div className={style['alert-button']}>
+                                <Button onClick={() => this.handleClickOk(this.props)} style={{ width: '100%' }} purely={true} theme={okBtnTheme}>{this.props.okText}</Button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+            case 'confirm': {
+                return (
+                    <div>
+                        {this.props.enableMask && <div className={style['dialog-mask']} onClick={() => this.handleClickMask(this.props)}></div>}
+                        <div className={`${style['at-dialog-alert']} ${style[theme]}`}>
+                            <div className={style['alert-title']}>{this.props.title}</div>
+                            <div className={style['alert-content']}>{this.props.content}</div>
+                            <div className={style['alert-button']}>
+                                <Button onClick={() => this.handleClickCancel(this.props)} style={{ width: '50%' }} purely={true} theme={cancelBtnTheme}>{this.props.cancelText}</Button>
+                                <Button onClick={() => this.handleClickOk(this.props)} style={{ width: '50%' }} purely={true} theme={okBtnTheme}>{this.props.okText}</Button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
             default: {
                 return null
             }
         }
     }
 }
-const destory = (div, props) => {
-    const id = div.getAttribute('id')
-    if (document.getElementById(id)) {
-        if (props.beforeClose && props.beforeClose(props)) {
-            ReactDOM.unmountComponentAtNode(div)
-            div.parentNode.removeChild(div)
-            props.afterClose && props.afterClose(props)
-        }
-    }
-}
-const renderDialog = (props) => {
-    let dialogContainer = document.getElementById(`dialog_${props.type}`)
-    if (!dialogContainer) {
-        dialogContainer = document.createElement('div')
-        dialogContainer.id = `dialog_${props.type}`
-        dialogContainer.className = style['at-dialog-toast-container']
-        document.body.appendChild(dialogContainer)
-    }
-    let dialogDIV = document.createElement('div')
-    dialogDIV.id = `dialog_${props.type}_${new Date().getTime()}`
-    dialogContainer.appendChild(dialogDIV)
-    props.id = dialogDIV.id
-    ReactDOM.render(<Dialog {...props} />, dialogDIV)
-    if (props.max && props.max > 0) {
-        if (dialogContainer.childNodes.length > props.max) {
-            destory(dialogContainer.firstChild, props)
-        }
-    }
-    if (props.delay && props.delay > 0) {
-        setTimeout(() => {
-            destory(dialogDIV, props)
-        }, props.delay)
-    }
-    return dialogDIV
-}
+
 
 export default {
     close: (ele) => {
@@ -95,10 +155,43 @@ export default {
         return ele
     },
     alert: (config) => {
+        let opts = Object.assign({
+            type: 'alert',
+            content: config.content || 'No content...',
+            title: config.title || 'No title',
+            enableCloseByMask: false,
+            enableMask: true,
+            beforeClose: () => {
+                return true;
+            },
+            afterClose: () => {
 
+            },
+            okText: 'Ok',
+            theme: 'default'
+        }, config)
+        const ele = renderDialog(opts)
+        return ele
     },
     confirm: (config) => {
+        let opts = Object.assign({
+            type: 'confirm',
+            content: config.content || 'No content...',
+            title: config.title || 'No title',
+            enableCloseByMask: false,
+            enableMask: true,
+            beforeClose: () => {
+                return true;
+            },
+            afterClose: () => {
 
+            },
+            okText: 'Ok',
+            cancelText: 'Cancel',
+            theme: 'default'
+        }, config)
+        const ele = renderDialog(opts)
+        return ele
     },
     modal: (config) => {
 
--- pages/ui/dialog.js
+++ pages/ui/dialog.js
@@ -8,11 +8,39 @@ class SpinnerPage extends React.Component {
     }
     handleOpenToast(t) {
         Dialog.toast('This is toast ' + new Array(Math.ceil(Math.random() * 60)).join('Haha '), {
-            afterClose: (props) => {
-                console.log('Oh, i close by delay time', props)
+            afterClose: (type, props) => {
+                console.log('Oh, i close by delay time', type)
             },
             theme: t,
-            delay: 100000
+            delay: 2000
+        })
+    }
+    handleOpenAlert(t) {
+        Dialog.alert({
+            title: 'Title: ' + t,
+            content: <div><p>hahah</p><p>heheeh</p></div>,
+            beforeClose: (type, props) => {
+                console.log('Before close by: ', type)
+                return true
+            },
+            afterClose: (type, props) => {
+                console.log('Oh, i close by: ', type)
+            },
+            theme: t
+        })
+    }
+    handleOpenConfirm(t) {
+        Dialog.confirm({
+            title: 'Title: ' + t,
+            content: <div><p>hahah</p><p>heheeh</p></div>,
+            beforeClose: (type, props) => {
+                console.log('Before close by: ', type)
+                return true
+            },
+            afterClose: (type, props) => {
+                console.log('Oh, i close by: ', type)
+            },
+            theme: t
         })
     }
     render() {
@@ -23,9 +51,32 @@ class SpinnerPage extends React.Component {
                         <Button size="small" theme="default" onClick={() => this.handleOpenToast('default')}>Open Toast default</Button>
                         <Button size="small" theme="success" onClick={() => this.handleOpenToast('success')}>Open Toast success</Button>
                         <Button size="small" theme="info" onClick={() => this.handleOpenToast('info')}>Open Toast info</Button>
+                        <Button size="small" theme="primary" onClick={() => this.handleOpenToast('primary')}>Open Toast primary</Button>
                         <Button size="small" theme="warning" onClick={() => this.handleOpenToast('warning')}>Open Toast warning</Button>
                         <Button size="small" theme="danger" onClick={() => this.handleOpenToast('danger')}>Open Toast danger</Button>
                         <Button size="small" theme="purple" onClick={() => this.handleOpenToast('purple')}>Open Toast purple</Button>
+                    </Col>
+                </Row>
+                <Row style={{marginTop: '16px'}}>
+                    <Col>
+                        <Button size="small" theme="default" onClick={() => this.handleOpenAlert('default')}>Open Alert default</Button>
+                        <Button size="small" theme="success" onClick={() => this.handleOpenAlert('success')}>Open Alert success</Button>
+                        <Button size="small" theme="info" onClick={() => this.handleOpenAlert('info')}>Open Alert info</Button>
+                        <Button size="small" theme="primary" onClick={() => this.handleOpenAlert('primary')}>Open Alert primary</Button>
+                        <Button size="small" theme="warning" onClick={() => this.handleOpenAlert('warning')}>Open Alert warning</Button>
+                        <Button size="small" theme="danger" onClick={() => this.handleOpenAlert('danger')}>Open Alert danger</Button>
+                        <Button size="small" theme="purple" onClick={() => this.handleOpenAlert('purple')}>Open Alert purple</Button>
+                    </Col>
+                </Row>
+                <Row style={{ marginTop: '16px' }}>
+                    <Col>
+                        <Button size="small" theme="default" onClick={() => this.handleOpenConfirm('default')}>Open Confirm default</Button>
+                        <Button size="small" theme="success" onClick={() => this.handleOpenConfirm('success')}>Open Confirm success</Button>
+                        <Button size="small" theme="info" onClick={() => this.handleOpenConfirm('info')}>Open Confirm info</Button>
+                        <Button size="small" theme="primary" onClick={() => this.handleOpenConfirm('primary')}>Open Confirm primary</Button>
+                        <Button size="small" theme="warning" onClick={() => this.handleOpenConfirm('warning')}>Open Confirm warning</Button>
+                        <Button size="small" theme="danger" onClick={() => this.handleOpenConfirm('danger')}>Open Confirm danger</Button>
+                        <Button size="small" theme="purple" onClick={() => this.handleOpenConfirm('purple')}>Open Confirm purple</Button>
                     </Col>
                 </Row>
             </div>
